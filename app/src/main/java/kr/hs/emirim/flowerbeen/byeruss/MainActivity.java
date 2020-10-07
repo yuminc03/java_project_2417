ad013e08369ab698dbe2045c66994ae125fbd466
Yumin Chu <s2019w20@e-mirim.hs.kr>
2020-10-08 00:40:03 +0900

    java project - 방 만들기, 방 찾기 Dialog가 database만들기에 적합하지 않아서 다시 수정했음

--- app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/MainActivity.java
@@ -6,9 +6,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -18,14 +20,13 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String IP_ADDRESS = "175.209.132.195";
+    private static String TAG = "phptest";
+
     private DrawerLayout drawerLayout;
     private View drawerView;
     private Button btn_find, btn_make;
     private Context context;
-    EditText input_room;
-    EditText input_time;
-    EditText input_place;
-    private Button okButton, cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,20 +36,21 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         drawerView = (View)findViewById(R.id.drawer);
 
-        btn_find = (Button)findViewById(R.id.btn_find);
-        btn_make = (Button)findViewById(R.id.btn_make);
+        btn_find = (Button)findViewById(R.id.btn_find);//방 찾기
+        btn_make = (Button)findViewById(R.id.btn_make);//방 만들기
+
 
         btn_make.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openMakeDialog();
+                show_mroom();
             }
         });
 
         btn_find.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openFindDialog();
+                show_froom();
             }
         });
 
@@ -68,6 +70,67 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    void show_mroom(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_dialog_custom, null);
+        builder.setView(view);
+        final EditText input_room = (EditText) view.findViewById(R.id.input_room);
+        final EditText input_time = (EditText) view.findViewById(R.id.input_time);
+        final EditText input_place = (EditText) view.findViewById(R.id.input_place);
+        Button btn_create = (Button)view.findViewById(R.id.btn_create);
+        Button btn_back = (Button)view.findViewById(R.id.btn_back);
+
+        final AlertDialog dialog = builder.create();
+
+        btn_create.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                int room_code_int = (int)(Math.random() * 1000000000);
+                String room_code = Integer.toString(room_code_int);
+
+                String roomCode = room_code;
+                String roomName = input_room.getText().toString();
+                String roomTime = input_time.getText().toString();
+                String roomPlace = input_place.getText().toString();
+
+                dialog.dismiss();//종료
+            }
+        });
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+    void show_froom(){
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_find_room, null);
+        builder2.setView(view);
+        final EditText textInputCode = (EditText)view.findViewById(R.id.text_input_code);
+        final Button btn_check = (Button)view.findViewById(R.id.btn_check);
+        final Button btn_cancel = (Button)view.findViewById(R.id.btn_cancel);
+
+        final AlertDialog dialog = builder2.create();
+
+        btn_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String RoomCode = textInputCode.getText().toString();
+            }
+        });
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        dialog.show();
     }
 
     @Override
@@ -105,13 +168,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
-    public void openFindDialog(){//모임 찾기 버튼기능
-        FindRoomActivity findRoomActivity = new FindRoomActivity();
-        findRoomActivity.show(getSupportFragmentManager(), "find room dialog");
-    }
-    public void openMakeDialog(){//모임 만들기 버튼기능
-        DialogCustom makeRoomActivity = new DialogCustom();
-        makeRoomActivity.show(getSupportFragmentManager(), "make room dialog");
-    }
 
 }
--- app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/room_make_DAO.java
+++
@@ -1,8 +0,0 @@
-package kr.hs.emirim.flowerbeen.byeruss;
-
-public class room_make_DAO {
-
-    public void make_room(room_make_DTO dto){
-
-    }
-}
