2f0e4f35ec7de51d241b3e7a42955876171be29b
Yumin Chu <s2019w20@e-mirim.hs.kr>
2020-10-22 23:24:10 +0900

    java project - SQLite에 데이터를 넣고 불러오는 작업

--- app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/RoomDataActivity.java
+++ app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/RoomDataActivity.java
@@ -7,13 +7,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.AlertDialog;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -27,7 +30,6 @@ public class RoomDataActivity extends AppCompatActivity {
     private String DB_PATH =  " /data/data/kr.hs.emirim.flowerbeen.byeruss/byeruss_room.db";
 
     ListView myRoomList = null;
-    Button mBtInsert = null;
 
     EditText input_id = null;
     EditText input_room = null;
@@ -48,12 +50,23 @@ public class RoomDataActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_data);
 
+        myRoomList = (ListView)findViewById(R.id.my_room_list);
+        myRoomList.setOnItemLongClickListener(mLongClickListener);
+
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerView = (View) findViewById(R.id.drawer);
 
         btn_find = (Button) findViewById(R.id.btn_find);//방 찾기
         btn_make = (Button) findViewById(R.id.btn_make);//방 만들기
 
+        input_id = (EditText) findViewById(R.id.input_id);
+        input_room = (EditText) findViewById(R.id.input_room);
+        input_time = (EditText) findViewById(R.id.input_time);
+        input_place = (EditText) findViewById(R.id.input_place);
+
+        if( mHandler == null ) {
+            mHandler = MyDBHandler.open(RoomDataActivity.this, DB_PATH);
+        }
 
         btn_make.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,15 +99,88 @@ public class RoomDataActivity extends AppCompatActivity {
         });
     }
 
+    AdapterView.OnItemLongClickListener mLongClickListener = new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+            mCursor.moveToPosition(position);
+            Log.d(TAG, "id : "+mCursor.getString(0) + "roomName : " + mCursor.getString(1));
+            mHandler.delete(mCursor.getString(1));
+
+            mCursor = mHandler.select();  // DB 새로 가져오기
+            mAdapter.changeCursor(mCursor); // Adapter에 변경된 Cursor 설정하기
+            mAdapter.notifyDataSetChanged(); // 업데이트 하기
+
+            return true;
+        }
+    };
+
+    void insertToDB() {
+        /*
+        try {
+            database = mh.getWritableDatabase();
+            sql = "select * from byeruss_make_room where roomId='"
+                    + id.getText().toString() + "';";
+            Cursor cur;
+            cur = database.rawQuery(sql, null);
+            Log.i(sql, "a");
+            int checkid;
+            checkid = cur.getCount();
+            Log.i(checkid + "", "a");
+            if (checkid == 0) {// 중복 아님
+                Log.i(sql, "a");
+                check = 0;
+                Toast.makeText(getApplicationContext(),
+                        "사용가능합니다", Toast.LENGTH_LONG).show();
+            }
+            if (checkid != 0) {
+                check = 1;
+                id.setText("");
+                Toast.makeText(getApplicationContext(),
+                        name.getText().toString()+"다시 입력해주세요", Toast.LENGTH_LONG).show();
+            }// 중복
+            cur.close();
+            database.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        
+         */
+
+        Log.d(TAG, "insertToDB");
+
+        String id, room, time, place;
+
+        if( (input_id.getText() != null) && (input_room.getText() != null)
+                && (input_time.getText() != null) && (input_place.getText() != null)) {
+            id = input_id.getText().toString();
+            room = input_room.getText().toString();
+            time = input_time.getText().toString();
+            place = input_place.getText().toString();
+        }
+        else {
+            Toast.makeText(RoomDataActivity.this, "Please enter the all members", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mHandler.insert(id, room, time, place);
+
+        mCursor = mHandler.select();  // DB 새로 가져오기
+        mAdapter.changeCursor(mCursor); // Adapter에 변경된 Cursor 설정하기
+        mAdapter.notifyDataSetChanged(); // 업데이트 하기
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.close();
+    }
+
     void show_mroom() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
         View view = inflater.inflate(R.layout.activity_dialog_custom, null);
         builder.setView(view);
-        final EditText input_id = (EditText) view.findViewById(R.id.input_id);
-        final EditText input_room = (EditText) view.findViewById(R.id.input_room);
-        final EditText input_time = (EditText) view.findViewById(R.id.input_time);
-        final EditText input_place = (EditText) view.findViewById(R.id.input_place);
         Button btn_create = (Button) view.findViewById(R.id.btn_create);
         Button btn_back = (Button) view.findViewById(R.id.btn_back);
 
@@ -108,7 +194,7 @@ public class RoomDataActivity extends AppCompatActivity {
                 String roomPlace = input_place.getText().toString();
 
                 try {
-
+                    insertToDB();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
