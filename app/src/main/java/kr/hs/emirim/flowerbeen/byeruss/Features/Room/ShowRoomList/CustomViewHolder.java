8bb9070e5530d873620feb01b829fb3746b4d02b
Yumin Chu <s2019w20@e-mirim.hs.kr>
2020-11-02 23:27:31 +0900

    java project - dialog 수정함, SQLite에 데이터를 넣고 불러오는 작업

--- app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/Features/Room/ShowRoomList/CustomViewHolder.java
+++ app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/Features/Room/ShowRoomList/CustomViewHolder.java
@@ -5,12 +5,21 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CustomViewHolder extends RecyclerView.ViewHolder{
+import kr.hs.emirim.flowerbeen.byeruss.R;
+
+public class CustomViewHolder extends RecyclerView.ViewHolder{//마치 list형식으로 보여주기
 
     TextView roomIdTextView;
     TextView roomNameTextView;
+    TextView roomTimeTextView;
+    TextView roomPlaceTextView;
 
     public CustomViewHolder(View itemView) {
         super(itemView);
+
+        //roomIdTextView = itemView.findViewById(R.id.roomIdTextView);
+        //roomNameTextView = itemView.findViewById(R.id.roomNameTextView);
+        //roomTimeTextView = itemView.findViewById(R.id.roomTimeTextView);
+        //roomPlaceTextView = itemView.findViewById(R.id.roomIdTroomPlaceTextViewsextView);
     }
 }
