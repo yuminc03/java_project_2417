3bac2d6432107ba34437451b7057a4a3994a76d0
Yumin Chu <s2019w20@e-mirim.hs.kr>
2020-11-30 15:50:46 +0900

    #11-30: checklist 삽입하기 도전!

---
+++ app/src/main/java/kr/hs/emirim/flowerbeen/byeruss/checkListActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.flowerbeen.byeruss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_list);
+    }
+}
